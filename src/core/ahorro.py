34d7b7f537ef54fcd1e857dd90b751080602f63e
Luis Guerra <mhenaoecheverri@gmail.com>
2026-02-18 08:55:42 -0500

    todos los casos de error en el codigo y condicionales

--- src/core/ahorro.py
+++ src/core/ahorro.py
@@ -1,15 +1,17 @@
 class AhorroProgramado:
     """
-        Documentacion AhorroProgramado
-        
-        :param meta: La meta a la cual se desea llegar
-        :type meta: float
-        :param plazo: El plazo en meses para alcanzar la meta
-        :type plazo: int
-        :param extra: El monto extra que se desea aportar cada mes
-        :type extra: float
-        :return: El monto que se debe ahorrar cada mes para alcanzar la meta en el plazo establecido, considerando el aporte extra
-        :rtype: float
+    Documentacion AhorroProgramado
+    
+    :param meta: La meta a la cual se desea llegar
+    :type meta: float
+    :param plazo: El plazo en meses para alcanzar la meta
+    :type plazo: int
+    :param extra: El monto extra que se desea aportar en un mes específico
+    :type extra: float
+    :param mes_extra: El mes en el que se hará el aporte extra
+    :type mes_extra: int
+    :return: El monto que se debe ahorrar cada mes para alcanzar la meta
+    :rtype: float
     """ 
 
     def __init__(self, meta: float, plazo: int, extra: float, mes_extra: int):
@@ -19,18 +21,21 @@ class AhorroProgramado:
         self.mes_extra = mes_extra
     
     def calcular_ahorro(self) -> float:
-        if self.plazo == 0:
-            return 0
-        ahorro_base = (self.meta - self.extra * self.plazo) / self.plazo
+        if self.meta < 0: 
+            return "Error: el valor a ahorar debe ser mayor a  0"
+        if self.plazo <= 0: 
+            return "Error: el plazo debe ser mayor a 0"
+        if self.mes_extra < 1 or self.mes_extra > self.plazo:
+            return "Error"          
+        if self.extra > self.meta: 
+            return "Error: abono supera meta de ahorro"
+        
+        meta_restante = self.meta - self.extra
+
+        ahorro_base = meta_restante / self.plazo
+        
+      
         return round(ahorro_base, 2) if ahorro_base > 0 else 0
+      
 
-
-
-meta1 = AhorroProgramado(1100000, 6, 0, 0)
-print(meta1.calcular_ahorro())
-    
-
-
-
- 
     
